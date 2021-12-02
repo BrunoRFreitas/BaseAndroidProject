7adad78a9e92fe15d8dd659b4d50967069d0fb7a
Bruno Freitas <bruno.freitas@meta.com.br>
2021-12-02 20:51:35 -0300

    refactor builds

--- app/src/main/java/com/brfdev/multimodulesbaseproject/MainActivity.kt
+++ app/src/main/java/com/brfdev/multimodulesbaseproject/MainActivity.kt
@@ -2,10 +2,18 @@ package com.brfdev.multimodulesbaseproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setVersionName(BuildConfig.VERSION_NAME)
+    }
+
+    private fun setVersionName(versionName: String) {
+        val tvVersao = findViewById<TextView>(R.id.tv_versao)
+        tvVersao.text = versionName
     }
 }
